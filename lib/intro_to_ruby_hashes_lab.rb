7926617c2021e6b1e41d52dfe06e6136eb21bce2
Phong Lo <phonglo724@gmail.com>
2020-12-22 21:11:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   {:name => "Grace Hopper"}
-  my_hash_creater[:name]# return a hash that includes the key and value parameters passed into this method
+  my_hash_creater[:name, "Grace Hooper"]# return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
