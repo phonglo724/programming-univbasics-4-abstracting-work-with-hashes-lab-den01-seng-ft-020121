b2ec399bf4022ab2e58f3b0a08179121fc56e750
Phong Lo <phonglo724@gmail.com>
2020-12-22 21:14:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def my_hash_creator(key, value)
- {:name, "Grace Hopper"}
+ my_hash_creator{:name, "George"}
  my_hash_creator[:id, 5]
  my_hash_creater["age", 39]
  # return a hash that includes the key and value parameters passed into this method
